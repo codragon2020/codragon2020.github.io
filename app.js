73c7d266d2c66f483f9b4b883a1094b80efc4177
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-01-24 11:05:00 -0500

    Refactored the html into app.js. Dynamically calling css. Adding logic to reload page on current page

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+// *****************Change CSS**************
+
+
+function changeCSS(cssFile, cssLinkIndex, backgroundUrl) {
+    // cssLinkIndex = 1;
+    var jumbotronEl = document.querySelector(".jumbotron")
+    jumbotronEl.style.backgroundImage = "url(" + backgroundUrl +")"
+    
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+    console.log(oldlink, cssLinkIndex)
+    var newlink = document.createElement("link");
+    console.log(newlink, cssLinkIndex)
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+    console.log('running css replace')
+}
+
 // **************Show Pages***************
 
 // Show About page and hides the rest
@@ -5,13 +26,14 @@ function showAbout() {
     aboutPage.style.display = "block";
     contactPage.style.display = "none";
     portfolioPage.style.display = "none";
-}
+    changeCSS('css/style.css', 1, 'images/background-tech-07.jpg');}
 
 // Show Contact page and hides the rest
 function showContact() {
     aboutPage.style.display = "none";
     contactPage.style.display = "block";
     portfolioPage.style.display = "none";
+    changeCSS('css/contact.css', 1, 'images/background-tech-12.jpg');
 }
 
 // Show Portfolio page and hides the rest
@@ -19,23 +41,35 @@ function showPortfolio() {
     aboutPage.style.display = "none";
     contactPage.style.display = "none";
     portfolioPage.style.display = "block";
+    changeCSS('css/portfolio.css', 1, 'images/background-tech-06.jpg');
 }
 
 // **************Click Events***************
 $("#about").on("click", function (e) {
     e.preventDefault();
     showAbout();
+    localStorage.setItem("lastpage", "about");
+
 })
 
 $("#contact").on("click", function (e) {
     e.preventDefault();
     showContact();
+    localStorage.setItem("lastpage", "contact");
 })
 
 $("#portfolio").on("click", function (e) {
     e.preventDefault();
     showPortfolio();
+    localStorage.setItem("lastpage", "portfolio");
 })
 
 // Load page
-showAbout();
+var lastPage = localStorage.getItem("lastpage")
+if (lastPage === "contact") {
+    showContact();
+} else if (lastPage === "portfolio") {
+    showPortfolio()
+} else showAbout();
+
+// showAbout();
